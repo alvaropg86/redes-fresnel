4f53787b804772758a248b91a616e45730000c96
alvaropg86 <82276886+alvaropg86@users.noreply.github.com>
2022-08-17 10:45:07 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,27 @@ function myFunction() {
   x = document.getElementById("dist").value;  
   y = document.getElementById("frec").value;
 
-
-  if (x == "" || y == ""){
-    text ="Aquí se expondrá el resultado"
+  if (x.includes(',') === true ){
+    x = x.replace(',', '.')
+  }
+  if (y.includes(',') === true ){
+    y = y.replace(',', '.')
   }
 
+  if (x == "" && y == ""){
+    text ="Aquí se expondrá el resultado"
+  }
+  if (x == "" || y == ""){
+    text ="Falta ingresar alguno de los datos solictados"}
   else if (isNaN(x) || isNaN(y)){  
-    text = "Algun dato ingresado no es correcto";  
-  } else {  
-    //si no ponemos parseFloat concatenaría x con y  
+    text = "Algun dato ingresado no es correcto"}
+  else if (y == '0'){
+      text = "La frecuencia no puede ser cero"
+    }
+  else {  
+    //si no ponemos parseFloat concatenaría x con y
     calculo = 8.656 * Math.sqrt(parseFloat(x)/parseFloat(y));  
     text= 'Resultado: ' + calculo.toFixed(2) + ' metros';  
   }
   document.getElementById("calculando").innerHTML = text;  
 }  
-
-
-
